8711c851841d532cbf1d450b7ef65cb09a97c766
Steyfix <tenryubitoserveur@gmail.com>
2022-08-30 18:58:51 +0200

    Update cs_quest.cpp

--- src/server/scripts/Commands/cs_quest.cpp
+++ src/server/scripts/Commands/cs_quest.cpp
@@ -185,6 +185,17 @@ public:
             handler->SetSentErrorMessage(true);
             return false;
         }
+                if (sWorld->getBoolConfig(CONFIG_QUEST_ENABLE_QUEST_TRACKER)) // check if Quest Tracker is enabled
+        {
+            // prepare Quest Tracker datas
+            PreparedStatement* stmt = CharacterDatabase.GetPreparedStatement(CHAR_UPD_QUEST_TRACK_GM_COMPLETE);
+            stmt->setUInt32(0, quest->GetQuestId());
+            stmt->setUInt32(1, player->GetGUIDLow());
+
+            // add to Quest Tracker
+            CharacterDatabase.Execute(stmt);
+        }
+
 
         player->CompleteQuest(entry, true, true);
         return true;
